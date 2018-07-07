b8e7e4ca65599f829dd96b31e80ea3f082f25352
Beck Chen <beckchen@BCHEN-MBP-SF.local>
2018-07-07 14:35:23 -0700

    WM-103[Beck] add unit tests for locationApi and weatherApi

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -6,3 +6,16 @@ Enzyme.configure({ adapter: new Adapter() });
 global.requestAnimationFrame = function (callback) {
     setTimeout(callback, 0);
 };
+
+const mockGeolocation = {
+    getCurrentPosition: (func) => {
+        return func({
+            coords: {
+                latitude: 32,
+                longitude: 122
+            }
+        });
+    }
+};
+
+global.navigator.geolocation = mockGeolocation;
